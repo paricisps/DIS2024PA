d3ab20c9c0a88b2a4544ce4b253ff29b7d8185cc
paricisps <25217@stpauls.qld.edu.au>
2023-01-31 17:21:22 +1000

    .

--- WordCounter.py
+++ WordCounter.py
@@ -1,1 +1,19 @@
 
+runProgram = True
+
+print("Hello and welcome to the title word counter checker.")
+
+while runProgram == True:
+    startProgram = input("Would you like to check if the word count of your book title is good for marketing? (yes/no) ").lower()
+    if startProgram == "yes":
+        title = input("Please input your book title. ")
+        titleLength = len(title.split())
+        print(f"Your book title contains {titleLength} words")
+        if titleLength <= 2:
+            print("Your book has good marketability! Congratulations! ")
+        else:
+            print("Your book title is too long. Try shortening it for better marketability. ")
+    else:
+        print("Thank you for using this program.")
+        runProgram = False
+
