d92db0b7695800d598b195ca9b07aee8bafd925b
paricisps <25217@stpauls.qld.edu.au>
2023-01-31 15:11:10 +1000

    .

--- ToDoList.py
+++ ToDoList.py
@@ -1,11 +1,20 @@
+runProgram = True
+print("Welcome to my book title counter!")
 
-#get user input as variable 'title'
+while runProgram == True:
+    startProgram = input("Would you like me to count the letters in the title of your book? (yes/no) ").lower()
+    if startProgram == "yes":
+        # get user input as variable 'title'
 
-title = input("Please input the title of your book. ")
+        title = input("Please input the title of your book. ")
 
-#store length of 'title' as 'titleLength'
+        # store length of 'title' as 'titleLength'
 
-titleNoSpace = title.replace(" ", "")
-titleLength = len(titleNoSpace)
+        titleNoSpace = title.replace(" ", "")
+        titleLength = len(titleNoSpace)
 
-print("There are " + str(titleLength) + " letters in " + title)
+        print(f"There are {str(titleLength)} letters in {title}, not including spaces")
+    else:
+        print("Thank you for using this program!")
+        runProgram = False
+
