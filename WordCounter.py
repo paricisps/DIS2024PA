56edfe603ba549b6b9ebfdc98d944b832d5c5740
paricisps <25217@stpauls.qld.edu.au>
2023-02-01 11:55:48 +1000

    .

--- WordCounter.py
+++ WordCounter.py
@@ -1,23 +1,35 @@
-runProgram = True
+runProgram = True #lets the loop run
 
-print("Hello and welcome to the title word counter checker.")
-titleHistory = []
-while runProgram == True:
-    startProgram = input("Would you like to check if the word count of your book title is good for marketing? (yes/no) ").lower()
-    if startProgram == "yes":
-        title = input("Please input your book title. ")
-        titleLength = len(title.split())
-        titleHistory.append(title.title())
-        print(f"The title of your book, {title.title()} contains {titleLength} words")
-        if titleLength <= 2:
+print("Hello and welcome to the title word counter checker.") # greets the user
+titleHistory = [] # creates list for books
+lengthHistory = [] # creates list for title Length
+
+while runProgram == True: # beginning of loop
+    startProgram = input("Would you like to check if the word count of your book title is good for marketing? (yes/no) ").lower() # asks the user if they want to start
+
+    if startProgram == "yes": # runs if user wants to start
+        title = input("Please input your book title. ") #lets user input title
+
+        titleLength = len(title.split()) # checks the number of words in the title
+
+        titleHistory.append(title.title()) # appends the title name to list
+        lengthHistory.append(titleLength) # appends the title length to list
+
+        print(f"The title of your book, {title.title()} contains {titleLength} words") # prints the words in the title
+
+        if titleLength <= 2: # checks if title is less than or equal to 2
             print("Your book has good marketability! Congratulations! ")
-        else:
+
+        else: # runs if title is more than 2
             print("Your book title is too long. Try shortening it for better marketability. ")
-    else:
+
+    else: # runs if user does not want to start program
         print("Thank you for using this program.")
+        print(50 * '~')
         print("The books you checked were: ")
 
-        for i in range(0, len(titleHistory)):
-            print(titleHistory[i])
-        runProgram = False
+        for i in range(0, len(titleHistory)): # runs loop for the amount of entries in the titleHistory list
+            print(f"{i+1}: {titleHistory[i]}, containing {lengthHistory[i]} words") # prints the title and its associated word length
+
+        runProgram = False # stops the loop from running again, ending the program
 
