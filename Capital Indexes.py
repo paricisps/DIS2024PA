6e3d0f3d7bcb6341d25ab2633af80b78069687b0
paricisps <25217@stpauls.qld.edu.au>
2023-02-03 13:34:50 +1000

    .

--- Capital Indexes.py
+++ Capital Indexes.py
@@ -1,12 +1,18 @@
+runProgram = True
 
-userInput = input("Please input a string. ")  # gets user input
-upperValues = []  # creates an empty list to store the placement of each uppercase letter
+while runProgram:
+    userInput = input("Please input a string or press q to quit ")  # gets user input
+    upperValues = []  # creates an empty list to store the placement of each uppercase letter
 
-for i in range (0, len(userInput)):  # runs program for the length of the user input
-    if userInput[i].isupper():  # checks if each letter is uppercase
-        upperValues.append(i)  # if uppercase, appends the index of the letter to upperValues
+    if userInput == "q" or userInput == "Q":
+        print("Thank you for using this program.")
+        runProgram = False
+    else:
+        for i in range (0, len(userInput)):  # runs program for the length of the user input
+            if userInput[i].isupper():  # checks if each letter is uppercase
+                upperValues.append(i)  # if uppercase, appends the index of the letter to upperValues
 
-if len(upperValues) > 0:
-    print(f"{userInput} contains uppercase characters at {upperValues}")  # prints the placement of the uppercase letters
-else:
-    print(f"'{userInput}' does not contain any uppercase letters.")
+        if len(upperValues) > 0:
+            print(f"{userInput} contains uppercase characters at {upperValues}")  # prints the placement of the uppercase letters
+        else:
+            print(f"'{userInput}' does not contain any uppercase letters.")
